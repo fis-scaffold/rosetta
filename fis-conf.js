05da0007064236f9cb77e17add0cc50f5cfc313d
2betop <liaoxuezhi@icloud.com>
2015-05-04 15:14:52 +0800

    update

--- fis-conf.js
+++ fis-conf.js
@@ -1,15 +1,39 @@
 fis.set('project.files', '*.html');
 
-fis.media('dev')
-  .match('**', {
-    useHash: false,
-    useDomain: false
-  });
-
-
 fis.match('*.html', {
   parser: fis.plugin('rosetta')
 });
+
+// default settings. fis3 release
+fis
+  .media('dev')
+
+  .match('*.{css,js,html}', {
+    useMap: true
+  });
+
+// fis3 release --media production
+fis
+  .media('production')
+
+  .match('*.js', {
+    optimizer: fis.plugin('uglify-js'),
+    useHash: true,
+    useDomain: true,
+    useMap: true
+  })
+
+  .match('*.css', {
+    optimizer: fis.plugin('clean-css'),
+    useHash: true,
+    useDomain: true,
+    useMap: true
+  })
+
+  .match('*.png', {
+    optimizer: fis.plugin('png-compressor')
+  });
+
 
 // 开启 autuload, 好处是，依赖自动加载。
 fis.set('modules.postpackager', fis.plugin('autoload'));
