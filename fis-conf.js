ce2937023981231d84c43f6bebbd57c66edfcf6b
2betop <liaoxuezhi@icloud.com>
2015-05-12 11:56:40 +0800

    update

--- fis-conf.js
+++ fis-conf.js
@@ -1,36 +1,47 @@
 fis.set('project.files', '*.html');
 
-fis.match('*.html', {
-  parser: fis.plugin('rosetta')
-});
-
 fis
+  .match('*.html', {
+    parser: fis.plugin('rosetta')
+  })
   .match('*.{css,js,html}', {
     useMap: true
+  })
+  .set('modules.postpackager', fis.plugin('rosetta'))
+
+// 在 dev 环境下关闭文件 md5 hash
+fis
+  .media('dev')
+  .match('*.{css,js,html}', {
+    useHash: false
   });
 
-// fis3 release --media production
+// fis3 release production
 fis
   .media('production')
 
+  // 压缩 js 通过 uglify
   .match('*.js', {
-    optimizer: fis.plugin('uglify-js'),
-    useHash: true,
-    useDomain: true,
-    useMap: true
+    optimizer: fis.plugin('uglify-js')
   })
 
+  // 压缩 css
   .match('*.css', {
-    optimizer: fis.plugin('clean-css'),
-    useHash: true,
-    useDomain: true,
-    useMap: true
+    optimizer: fis.plugin('clean-css')
   })
 
+  // 压缩 png
   .match('*.png', {
     optimizer: fis.plugin('png-compressor')
-  });
+  })
 
+  // 通过 rosetta 插件优化资源。
+  .set('modules.postpackager', fis.plugin('rosetta', {
 
-// 开启 autuload, 好处是，依赖自动加载。
-fis.set('modules.postpackager', fis.plugin('autoload'));
+    // 合并零碎文件
+    allInOne: true,
+
+    // 收集合并 脚本和样式文件。默认只会收集依赖，开启此选项后，页面中所有 js 和 css 都会被收集和合并。
+    obtainScript: true,
+    obtainStyle: true
+  }));
